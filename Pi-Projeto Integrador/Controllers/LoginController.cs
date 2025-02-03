5e12a9e594435ddb52437a5a0084db95851b2ec0
RaYaS <rafael.cesar.souza@hotmail.com>
2025-02-03 17:10:23 -0300

    primeiro commit

---
+++ Pi-Projeto Integrador/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pi_Projeto_Integrador.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Pi-Projeto Integrador/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pi_Projeto_Integrador.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
